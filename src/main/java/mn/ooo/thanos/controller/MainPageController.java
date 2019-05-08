e44548da77ec5d505e1d53dc4ed9d316e57554c9
Ucm <xyzucm@gmail.com>
2019-05-07 22:22:12 -0700

    Revert "Add Vue welcome page"
    This reverts commit d05601dcfa9de4539eb350c54a68c84b318a92f2.

--- src/main/java/mn/ooo/thanos/controller/MainPageController.java
+++ src/main/java/mn/ooo/thanos/controller/MainPageController.java
@@ -5,5 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainPageController {
-    
+
+    private static final String home = "/";
+
+    @RequestMapping(value = home)
+    public String home() {
+        return "Hello World!";
+    }
 }
