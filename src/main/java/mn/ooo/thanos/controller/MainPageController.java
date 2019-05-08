d05601dcfa9de4539eb350c54a68c84b318a92f2
Ucm <xyzucm@gmail.com>
2019-05-07 22:02:36 -0700

    Add Vue welcome page

--- src/main/java/mn/ooo/thanos/controller/MainPageController.java
+++ src/main/java/mn/ooo/thanos/controller/MainPageController.java
@@ -5,11 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainPageController {
-
-    private static final String home = "/";
-
-    @RequestMapping(value = home)
-    public String home() {
-        return "Hello World!";
-    }
+    
 }
